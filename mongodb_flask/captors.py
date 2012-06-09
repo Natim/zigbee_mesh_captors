92e78ddf0a59fcc00210aabb3ff50f2454cc0be0
Rémy HUBSCHER <remy.hubscher@utbm.fr>
2012-06-09 15:44:55 +0200

    Automatically drop old value in fake_listening. Improved the gauge_api mongodb call

--- mongodb_flask/captors.py
+++ mongodb_flask/captors.py
@@ -3,27 +3,20 @@ from flask import *
 from pymongo import Connection, ASCENDING, DESCENDING
 from collections import defaultdict
 from bson.code import Code
+from time import time
 
 app = Flask(__name__)
 db = Connection().mesh
 
+db.mesh_captors.ensure_index([('arduino', ASCENDING), ('pin', ASCENDING), ('date', DESCENDING)])
+
 @app.route('/')
 def index():
-    mapper = Code("""
-                  function () {
-                      emit(this.arduino, 1);
-                  }
-                  """)
-    reducer = Code("""
-                  function (key, values) {
-                      return 1;
-                  }
-                  """)
-    try:
-        arduinos = db.mesh_captors.map_reduce(mapper, reducer, "arduinos").find()
-        arduinos_id = [(int(x['_id']), str(hex(int(x['_id'])))) for x in arduinos]
+    arduinos = db.mesh_captors.distinct('arduino')
+    arduinos_id = [(x, str(hex(x))) for x in arduinos]
+    if len(arduinos_id) > 0:
         return render_template('index.html', arduinos=arduinos_id)
-    except:
+    else:
         return render_template('nodata.html', database=db.name)
 
 @app.route('/', methods=['POST'])
@@ -37,19 +30,25 @@ def gauge(arduino_id):
 
 @app.route('/gauge_api/<arduino_id>/')
 def gauge_api(arduino_id):
-    captors_value = db.mesh_captors.find({'arduino': int(arduino_id)}, 
-                                         sort=[('pin', ASCENDING), 
-                                               ('date', DESCENDING)])
-    captors = {}
+    # We keep only the last value ordered by date
+    reducer = Code("""
+                  function (doc, out) {
+                      if(out.date == 0 || out.date < doc.date) {
+                           out.date = doc.date;
+                           out.value = doc.value;
+                      }
+                  }
+                  """)
 
-    for captor in captors_value:
-        pin = int(captor['pin'])
-        if not pin in captors:
-            captors[pin] = captor
-
-    captors_values = [['Pin %s' % int(x['pin']), int(x['value'])] for x in captors.values()]
-
-    response = make_response(json.dumps(captors_values))
+    # We group lines by pin of the arduino
+    captors_values = db.mesh_captors.group(
+        key=['pin'], 
+        condition={'arduino': int(arduino_id)}, 
+        reduce=reducer, initial={'date': 0})
+    
+    response = make_response(json.dumps([
+                [u'Pin %s' % x['pin'], int(x['value'])] \
+                    for x in captors_values]))
     response.headers['Content-Type'] = 'application/json; charset=utf-8'
     return response
 
@@ -66,13 +65,15 @@ def line_api(arduino_id):
     captors = {}
     values = defaultdict(int);
     pins = []
-    
+
+    last_pin_value = defaultdict(int)
+
     for captor in captors_value:
         pin_name = 'Pin %d' % int(captor['pin'])
         if pin_name not in pins:
             pins.append(pin_name)
         values[pin_name] = int(captor['value'])
-        captors[captor['date'].strftime('%M%S')] = defaultdict(int, values)
+        captors[captor['date'].strftime('%H%M%S.%f')[1:8]] = dict(values)
 
     captors_values = []
     sorted_captors = sorted(captors.iteritems(), key=lambda x: x[0])
@@ -81,10 +82,15 @@ def line_api(arduino_id):
     for time, values_dict in sorted_captors:
         result = [time]
         for pin in pins:
-            result.append(values_dict[pin])
+            try:
+                result.append(values_dict[pin])
+                last_pin_value[pin] = values_dict[pin]
+            except KeyError:
+                result.append(last_pin_value[pin])
         captors_values.append(result)
-
-    response = make_response(json.dumps(captors_values))
+    
+    captors = [captors_values[0]] + captors_values[1:][-50:]
+    response = make_response(json.dumps(captors))
     response.headers['Content-Type'] = 'application/json; charset=utf-8'
     return response
 
--- mongodb_flask/fake_listening.py
+++ mongodb_flask/fake_listening.py
@@ -1,13 +1,15 @@
 # -*- coding: utf-8 -*-
 from pymongo import Connection
 import random
-from time import sleep
+from time import sleep, time
 import datetime
 
 db = Connection().mesh
 
 arduinos = [0xCB]
 pins = [14, 15, 16, 17, 18]
+
+starttime = time()
 
 try:
     while True:
@@ -21,5 +23,11 @@ try:
         infos['date'] = datetime.datetime.utcnow()
         info = db.mesh_captors.save(infos)
         print info, infos
+
+        if (time() - starttime) > 3*60:
+            print "\n\nCLEAR OLD DATA\n\n"        
+            db.mesh_captors.remove({'date': {'$lt': datetime.datetime.utcnow() - datetime.timedelta(seconds=360)}})
+            starttime = time()
+    
 except KeyboardInterrupt:
     exit(0)
